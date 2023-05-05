76c17a2da639e63d87865ff85824e3cc9a908011
hoyyChoi <hoyeon8165@gmail.com>
2023-05-06 02:44:03 +0900

    [feat] 로그인 유무 상태값

--- src/atoms/auth.jsx
+++ src/atoms/auth.jsx
@@ -1,5 +1,13 @@
 import {atom} from 'recoil'
 
+
+export const authState = atom({
+    key:'src/atoms/auth.jsx-authState',
+    default:false
+    
+})
+
+
 export const userState = atom({
     key: 'src/atoms/auth.jsx-userState', 
     default: {
