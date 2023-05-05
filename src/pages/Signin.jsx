71bb329403ca58557c9c9d7b52f494734c863417
hoyyChoi <hoyeon8165@gmail.com>
2023-05-06 02:44:49 +0900

    [feat] 로그인 & 로그아웃 서버연결

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { userState } from '../atoms/auth';
-import { useRecoilState,useSetRecoilState } from 'recoil'
+import { useRecoilState,useSetRecoilState } from 'recoil';
+import { postLoginUser } from '../remotes';
+import { authState } from '../atoms/auth';
 
 const Signin = () => {
 
@@ -14,6 +16,8 @@ const Signin = () => {
   const [email,setEmail] = useState('')
   const [password,setPw] = useState('')
   const [user,setUserState] = useRecoilState(userState)
+  const setAuth =  useSetRecoilState(authState)
+
 
   const spaceHome = () =>{
     navigate('/')
@@ -21,9 +25,22 @@ const Signin = () => {
 
   const LoginSubmit =(e)=>{
     e.preventDefault();
-    navigate('/')
-   
+    console.log(email)
+    console.log(password)
+    postLoginUser(email,password)
+    .then(res=>{
+      console.log(res)
+      console.log(res.data.data.accessToken)
+      console.log(res.data.data.refreshToken)
+      localStorage.setItem("accessToken",res.data.data.accessToken)
+      localStorage.setItem("refreshToken",res.data.data.refreshToken)
+      setAuth(true)
+      navigate('/')
+    })
+    .catch((err)=>console.log(err))
+
   }
+
 
   return (
     <div style={{display:'flex', justifyContent:"center",alignItems:'center',height:'500px'}}>
@@ -32,7 +49,7 @@ const Signin = () => {
         <Form style={{width:'500px'}} onSubmit={(e)=>LoginSubmit(e)}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" style={{fontSize:'20px'}} onChange={e=>setEmail(e.target.value)} />
+            <Form.Control type="text" placeholder="Enter email" style={{fontSize:'20px'}} onChange={e=>setEmail(e.target.value)} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
