f9be53ea38be6d98e6c0bea4d69cd85a275c994b
hoyyChoi <hoyeon8165@gmail.com>
2023-05-25 23:29:22 +0900

    [fix] challenge Page css 수정

--- src/pages/Challenge.jsx
+++ src/pages/Challenge.jsx
@@ -398,7 +398,11 @@ function Challenge() {
               <h2>질문 템플릿 상세 검색</h2>
               <div className="currentTemplateContainer">
                 {writeChallenge.templateData?.templates?.map((t, idx) => (
-                  <div className="currentTemplate" key={idx}>
+                  <div
+                    className="currentTemplate"
+                    key={idx}
+                    onClick={(e) => handlePlusClick(t, idx)}
+                  >
                     <div
                       className={
                         t.templateTitle.length >= 16 ? "twoline" : "oneline"
@@ -665,8 +669,14 @@ const Container = styled.div`
     text-align: center;
     padding: 15px;
     background: #f3f5f9;
-    box-shadow: 0px 18px 20px -18px rgba(39, 39, 39, 0.2);
+
     border-radius: 8px;
+    cursor: pointer;
+  }
+
+  .currentTemplate:hover {
+    box-shadow: 0px 18px 20px -18px rgba(39, 39, 39, 0.2);
+    margin-top: -3px;
   }
 
   .currentTemplate .oneline {
