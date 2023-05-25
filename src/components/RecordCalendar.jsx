e97a97d27e52e20483e8d3aa27315c91ca3338ee
백주연 <juurom@baegjuyeon-ui-MacBookAir.local>
2023-05-25 21:29:12 +0900

    [style] 달력 펼치기 버튼 가로길이 늘림

--- src/components/RecordCalendar.jsx
+++ src/components/RecordCalendar.jsx
@@ -129,7 +129,7 @@ const CalendarWrapper = styled.div`
     margin-right: 24px;
   }
   .topbar button {
-    width: 123px;
+    width: 140px;
     height: 40px;
     padding: 8px;
     font-size: 14px;
