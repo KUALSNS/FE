c219827f8cfc31baecd321b06e4c5065f42ca5a2
hoyyChoi <hoyeon8165@gmail.com>
2023-08-03 03:15:15 +0900

    safari 크로스부라우징 이슈 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,127 +1,124 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home";
-import Signin from "./pages/Signin";
-import Signup from "./pages/Signup";
-import Challenge from "./pages/Challenge";
+import {Routes, Route, useLocation} from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Challenge from './pages/Challenge';
 
-import Record from "./pages/Record";
-import Mypage from "./pages/Mypage";
-import FindIdPw from "./pages/FindIdPw";
-import LeftNav from "./components/navigation/LeftNav";
-import { getAccessToken } from "./remotes";
-import { useRecoilState } from "recoil";
-import { authState, challengeToastState, SmallScreenState } from "./atoms/auth";
-import jwt_decode from "jwt-decode";
-import { useEffect } from "react";
-import Navigation from "./components/navigation/Navigation";
-import ChallengeToast from "./components/toast/ChallengeToast";
+import Record from './pages/Record';
+import Mypage from './pages/Mypage';
+import FindIdPw from './pages/FindIdPw';
+import LeftNav from './components/navigation/LeftNav';
+import {getAccessToken} from './remotes';
+import {useRecoilState} from 'recoil';
+import {authState, challengeToastState, SmallScreenState} from './atoms/auth';
+import jwt_decode from 'jwt-decode';
+import {useEffect} from 'react';
+import Navigation from './components/navigation/Navigation';
+import ChallengeToast from './components/toast/ChallengeToast';
 
 function App() {
-  const location = useLocation();
-  const [auth, setAuth] = useRecoilState(authState);
-  const [isSmallScreen, setIsSmallScreen] = useRecoilState(SmallScreenState);
+	const location = useLocation();
+	const [auth, setAuth] = useRecoilState(authState);
+	const [isSmallScreen, setIsSmallScreen] = useRecoilState(SmallScreenState);
 
-  const [toast, setToast] = useRecoilState(challengeToastState);
+	const [toast, setToast] = useRecoilState(challengeToastState);
 
-  const checkTokenExpiration = () => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken) {
-      // 토큰의 만료 시간을 체크
-      const tokenExpiration = jwt_decode(accessToken).exp;
-      console.log("zz");
-      const currentTime = Date.now() / 1000;
-      // 토큰이 만료되었다면, 새로운 토큰을 발급
-      if (tokenExpiration < currentTime) {
-        getAccessToken()
-          .then((res) => {
-            console.log(res);
-            localStorage.setItem("accessToken", res.data.data.accessToken);
-            console.log("access 토큰 만 재발급");
-          })
-          .catch((error) => {
-            if (error.response.data.code === 419) {
-              localStorage.removeItem("accessToken");
-              localStorage.removeItem("refreshToken");
-              alert("로그인을 다시 하세요");
-              setAuth(false);
-            } else {
-              console.log(error);
-            }
-          });
-      }
-    }
-  };
+	const checkTokenExpiration = () => {
+		const accessToken = localStorage.getItem('accessToken');
+		if (accessToken) {
+			// 토큰의 만료 시간을 체크
+			const tokenExpiration = jwt_decode(accessToken).exp;
 
-  useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken) {
-      setAuth(true);
-    }
+			const currentTime = Date.now() / 1000;
+			// 토큰이 만료되었다면, 새로운 토큰을 발급
+			if (tokenExpiration < currentTime) {
+				getAccessToken()
+					.then(res => {
+						localStorage.setItem('accessToken', res.data.data.accessToken);
+					})
+					.catch(error => {
+						if (error.response.data.code === 419) {
+							localStorage.removeItem('accessToken');
+							localStorage.removeItem('refreshToken');
+							alert('로그인을 다시 하세요');
+							setAuth(false);
+						} else {
+							console.log(error);
+						}
+					});
+			}
+		}
+	};
 
-    function handleResize() {
-      setIsSmallScreen(window.innerWidth <= 1390);
-    }
+	useEffect(() => {
+		const accessToken = localStorage.getItem('accessToken');
+		if (accessToken) {
+			setAuth(true);
+		}
 
-    window.addEventListener("resize", handleResize);
-    handleResize();
+		function handleResize() {
+			setIsSmallScreen(window.innerWidth <= 1390);
+		}
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+		window.addEventListener('resize', handleResize);
+		handleResize();
 
-  useEffect(() => {
-    console.log(345);
-    if (toast) {
-      setTimeout(() => {
-        setToast(false);
-      }, 1500);
-    }
-  }, [toast]);
-  return (
-    <div>
-      {location.pathname == "/login" ||
-      location.pathname == "/find" ||
-      location.pathname == "/register" ? (
-        ""
-      ) : (
-        <div>
-          <Navigation />
-          {!isSmallScreen ? (
-            <div>
-              <LeftNav />
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
-      )}
-      {toast === "이미 진행 중인 챌린지에요!" ? (
-        <ChallengeToast message={toast} />
-      ) : toast === "진행중인 챌린지가 없어요!" ? (
-        <ChallengeToast message={toast} />
-      ) : toast === "오늘은 모두 다 작성하셨어요!" ? (
-        <ChallengeToast message={toast} />
-      ) : toast === "더 이상 챌린지를 진행할 수 없어요!" ? (
-        <ChallengeToast message={toast} />
-      ) : toast === "제목을 입력해주세요!" ? (
-        <ChallengeToast message={toast} />
-      ) : (
-        ""
-      )}
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/challenge" element={<Challenge />} />
+	useEffect(() => {
+		if (toast) {
+			setTimeout(() => {
+				setToast(false);
+			}, 1500);
+		}
+	}, [toast]);
+	return (
+		<div>
+			{location.pathname == '/login' ||
+			location.pathname == '/find' ||
+			location.pathname == '/register' ? (
+				''
+			) : (
+				<div>
+					<Navigation />
+					{!isSmallScreen ? (
+						<div>
+							<LeftNav />
+						</div>
+					) : (
+						''
+					)}
+				</div>
+			)}
+			{toast === '이미 진행 중인 챌린지에요!' ? (
+				<ChallengeToast message={toast} />
+			) : toast === '진행중인 챌린지가 없어요!' ? (
+				<ChallengeToast message={toast} />
+			) : toast === '오늘은 모두 다 작성하셨어요!' ? (
+				<ChallengeToast message={toast} />
+			) : toast === '더 이상 챌린지를 진행할 수 없어요!' ? (
+				<ChallengeToast message={toast} />
+			) : toast === '제목을 입력해주세요!' ? (
+				<ChallengeToast message={toast} />
+			) : (
+				''
+			)}
 
-        <Route path="/record" element={<Record />} />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/challenge" element={<Challenge />} />
 
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/login" element={<Signin />} />
-        <Route path="/register" element={<Signup />} />
-        <Route path="/find" element={<FindIdPw />} />
-      </Routes>
-    </div>
-  );
+				<Route path="/record" element={<Record />} />
+
+				<Route path="/mypage" element={<Mypage />} />
+				<Route path="/login" element={<Signin />} />
+				<Route path="/register" element={<Signup />} />
+				<Route path="/find" element={<FindIdPw />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
