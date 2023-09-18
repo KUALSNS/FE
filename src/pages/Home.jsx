6eba0750bd367d733b013e9d5783320cbc22f1e7
hoyyChoi <hoyeon8165@gmail.com>
2023-09-19 01:08:32 +0900

    fix : localStorage time

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -62,22 +62,9 @@ const Home = () => {
 				.catch(err => console.log(err));
 		}
 		const objString = window.localStorage.getItem('guideModal');
-
-		// null 체크
-		if (!objString) {
-			return null;
-		}
-
-		// 문자열을 객체로 변환
 		const obj = JSON.parse(objString);
-
-		// 현재 시간과 localStorage의 expire 시간 비교
-		if (Date.now() > obj.expire) {
-			// 만료시간이 지난 item 삭제
+		if (Date.now() > obj?.expire) {
 			window.localStorage.removeItem('guideModal');
-
-			// null 리턴
-			return null;
 		}
 	}, []);
 
